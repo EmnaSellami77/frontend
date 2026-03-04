4a81ab6e9cd357fdc1044d0b8ce66e839bb6a7e4
emnahajri37-code <emnahajri37@gmail.com>
2026-03-04 01:14:27 +0100

    zzzz

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,6 @@ function Footer() {
   return (
     <footer className="bg-dark text-white text-center p-3 mt-4">
       © 2026 IT Support Classification System PFE – Capgemini Engineering
-     
     </footer>
   );
 }
