f07470254ef93745fd98323d633e11fda5dccf5e
EmnaSellami77 <emnasellami18@gmail.com>
2026-03-05 23:54:27 +0100

    test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,12 @@ function Navbar() {
   return (
     <nav className="navbar navbar-dark bg-dark sticky-top">
       <div className="container-fluid">
-        <span className="navbar-brand mb-0 h1"></span>
-     
+        <span className="navbar-brand mb-0 h1">IT Support System</span>
       </div>
     </nav>
   );
 }
 
 export default Navbar;
+//test navbar   
+//teeessttt
