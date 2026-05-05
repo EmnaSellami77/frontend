ac2333078ccfd338365a157a8f295ce04574b863
emnahajri37-code <emnahajri37@gmail.com>
2026-05-06 00:16:39 +0100

    fix: correct backend URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://back2.onrender.com',
+  baseURL: 'https://back2-ys67.onrender.com',  // ← AJOUTE "-ys67"
   headers: { 'Content-Type': 'application/json' }
 });
 
